4f1cee8be84b3a1e65cb92d91d3cfb4e822a5ef1
alyonka <e.zhmudenko@gmail.com>
2025-05-17 11:50:15 -0400

    HW02

--- js/task-4.js
+++ js/task-4.js
@@ -21,4 +21,8 @@ function getShippingCost(country) {
 }
 console.log(getShippingCost("Australia"));
 console.log(getShippingCost("Germany"));
+console.log(getShippingCost("China"));
 console.log(getShippingCost("Chile"));
+console.log(getShippingCost("Jamaica"));
+console.log(getShippingCost("Sweden"));
+
